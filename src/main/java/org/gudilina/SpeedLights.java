3f8b511545c4c8beb287ae6c0a637b88a2e40925
Alyona Gudilina <gardwiz@yandex.ru>
2022-10-21 00:26:59 +0300

    add class with test

---
+++ src/main/java/org/gudilina/SpeedLights.java
@@ -0,0 +1,20 @@
+package org.gudilina;
+
+public class SpeedLights {
+
+    private final int speedLimit;
+
+    public SpeedLights(int speedLimit) {
+        this.speedLimit = speedLimit;
+    }
+
+    public String showLight(int currentSpeed) {
+        if (currentSpeed <= speedLimit) {
+            return "green";
+        } else if ((currentSpeed - speedLimit) <= 20){
+            return "yellow";
+        } else {
+            return "red";
+        }
+    }
+}
---
+++ src/test/java/org/gudilina/SpeedLightsTest.java
@@ -0,0 +1,40 @@
+package org.gudilina;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(Parameterized.class)
+public class SpeedLightsTest {
+
+    private final int currentSpeed;
+    private final String light;
+
+
+    public SpeedLightsTest(int currentSpeed, String light) {
+        this.currentSpeed = currentSpeed;
+        this.light = light;
+    }
+
+
+    @Parameters
+    public static Object[][] setSpeedData() {
+            return new Object[][] {
+                    {59, "green"},
+                    {61, "yellow"},
+                    {79, "yellow"},
+                    {81, "red"},
+            };
+    }
+
+    @Test
+    public void showLightWhenSpeed() {
+        SpeedLights lights = new SpeedLights(60);
+        String value = lights.showLight(currentSpeed);
+        assertEquals(light, value);
+    }
+}
